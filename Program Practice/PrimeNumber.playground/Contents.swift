1d70a25551930efa278fd684efedb01ba7b4645d
Mishra <Vinodkumarshrivastawa@gmail.com>
2023-03-25 11:20:40 +0530

    Adding Program for finding prime number

---
+++ Program Practice/PrimeNumber.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+//prime number
+
+func checkPrimeNumber(number: Int) -> Bool{
+    var isPrime = true
+    for i in 2 ... Int(sqrt(Double(number))) {
+        if number % i == 0 {
+            isPrime = false
+            break
+        }
+    }
+    return isPrime
+}
+
+let number = 71
+let number2 = 7834
+checkPrimeNumber(number: number)
